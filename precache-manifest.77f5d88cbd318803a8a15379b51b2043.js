cfe20bc1a45034fe70c991f80fd601c8cccac5ed
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-07-27 15:13:06 +0200

    Updates

---
+++ precache-manifest.77f5d88cbd318803a8a15379b51b2043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e772fda5c09ecc8a6dd557cc08a02d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67015dfaf67ee7ebb882",
+    "url": "/static/css/main.b80dcc9a.chunk.css"
+  },
+  {
+    "revision": "e697ac535b9422768626",
+    "url": "/static/js/2.8ee01a16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8ee01a16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67015dfaf67ee7ebb882",
+    "url": "/static/js/main.64b956d7.chunk.js"
+  },
+  {
+    "revision": "90ca52a47b0945b8fbe6",
+    "url": "/static/js/runtime-main.cc9a1c57.js"
+  }
+]);
